4d2969adc2751ed2c8b73bfd02947cb2a4e55264
Mawi Man (Ayoub Alarjani) <185532606+mawi-officiel@users.noreply.github.com>
2025-07-27 13:02:24 +0100

    Update script.js

--- web/cin/js/script.js
+++ web/cin/js/script.js
@@ -1,4 +1,4 @@
-// Cookie management functions
+// وظائف إدارة الكوكيز
 function setCookie(name, value, days) {
     const expires = new Date();
     expires.setTime(expires.getTime() + (days * 24 * 60 * 60 * 1000));
